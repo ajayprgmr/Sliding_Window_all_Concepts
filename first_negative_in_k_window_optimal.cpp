02bc3b734ea74a3ea62691258037e4c97c637806
Ajay Kushwaha <ak383834@gmail.com>
2023-02-20 00:35:13 +0530

    Create first_negative_in_k_window_optimal.cpp

---
+++ first_negative_in_k_window_optimal.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+// #include "Myutilities.h"
+using namespace std;
+vector<int> firstNegativeInKSizeWindow(vector<int>& v, int k) {
+    queue<int> q;
+    vector<int> ans;
+    int i = 0, j = 0, n = v.size();
+    while (j < n) {
+        if (v[j] < 0) q.push(v[j]);
+        if (j - i + 1 == k) {
+            if (q.empty()) ans.push_back(0);
+            else {
+                ans.push_back(q.front());
+                 if (v[i] == q.front()) q.pop();
+            }
+            i++;
+        }
+        j++;
+    }
+    return ans;
+}
+int main(){
+    int n;cin>>n;
+    int k;cin>>k;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    // for(auto i:arr)
+    // cout<<i<<" ";
+   vector<int>result=firstNegativeInKSizeWindow(arr,k);
+   for(auto i:result)
+   cout<<i<<" ";
+}

8d056e9d54947b7b8fb529cd5d74ab3f0405cea5
Ajay Kushwaha <ak383834@gmail.com>
2023-02-19 20:08:40 +0530

    Create first_negative_in_k_window_bruteforece.cpp

---
+++ first_negative_in_k_window_bruteforece.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+// #include "Myutilities.h"
+using namespace std;
+void FIRST_NEGATIVE_IN_K_SIZE_WINDOW(vector<int> &v ,int k){
+   int outerloop=v.size()-k;
+    for(int i=0;i<=outerloop;i++){
+         bool found=false;
+          int n=i+k;
+        for(int j=i;j<n;j++){
+            if(v[j]<0){
+            cout<<v[j]<<" ";
+            found=true;
+            break;
+            }
+        }
+
+        if(!found)
+        cout<<"0 ";
+    }
+
+}
+int main(){
+    int n;cin>>n;
+    int k;cin>>k;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    // for(auto i:arr)
+    // cout<<i<<" ";
+    FIRST_NEGATIVE_IN_K_SIZE_WINDOW(arr,k);
+
+   
+}
